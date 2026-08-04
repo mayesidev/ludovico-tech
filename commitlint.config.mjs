9b0b6402456dbf51274265fd92af1895520348c4
Isaac Mayes <github@mayesid.com>
2026-08-04 09:31:24 -0500

    ci: allow generated squash body length

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,8 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   ignores: [(message) => message.startsWith("Merge ")],
+  rules: {
+    // GitHub-generated squash bodies can exceed the conventional 100-character limit.
+    "body-max-line-length": [0, "always"],
+  },
 };
